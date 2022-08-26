ac63e48810fee2042a5aee462bd8f0bdf552c0d8
Andrea <andrea.delligatti96@gmail.com>
2022-08-26 17:44:08 +0200

    balanced parentheses problem

---
+++ balanced-parentheses/index.ts
@@ -0,0 +1,20 @@
+function balancedParentheses(str: string): boolean {
+    let curly = 0;
+    let par = 0;
+    let square = 0;
+
+    for(let i = 0; i < str.length; i++){
+        switch(str[i]){
+            case "(": par += 1; break;
+            case ")": par -= 1; break;
+            case "[": square += 1; break;
+            case "]": square -= 1; break;
+            case "{": curly += 1; break;
+            case "}": curly -= 1; break;
+        }
+    }
+
+    return curly == 0 && par == 0 && square == 0
+}
+
+console.log(balancedParentheses("((()){)}}{[]]"));
