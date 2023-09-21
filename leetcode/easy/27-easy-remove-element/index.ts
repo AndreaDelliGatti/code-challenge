2c3a38d110bb155c63de8ed921ac8f0afd6d436c
Andrea Delli Gatti <andrea.delligatti96@gmail.com>
2023-09-21 09:29:30 +0200

    leetocde 27 easy

---
+++ leetcode/easy/27-easy-remove-element/index.ts
@@ -0,0 +1,12 @@
+function removeElement(nums: any[], val: number): number {
+    let k = 0;
+    for (let i = 0; i < nums.length; i++)
+        if (nums[i] != val) {
+            nums[k] = nums[i];
+            k += 1;
+        }
+
+    return k;
+};
+
+console.log(removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2));
